e656a5005a1263761b4c7928da15f20810e55954
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-04-23 14:55:29 +0700

    oops

--- src/main/kotlin/cc/woverflow/easeify/Easeify.kt
+++ src/main/kotlin/cc/woverflow/easeify/Easeify.kt
@@ -1,15 +1,19 @@
 package cc.woverflow.easeify
 
 import net.fabricmc.api.ClientModInitializer
+import java.io.File
 
 class Easeify : ClientModInitializer {
     override fun onInitializeClient() {
-        println("Hello Fabric world!")
+        if (!modDir.exists()) {
+            modDir.mkdirs()
+        }
     }
 
     companion object {
         const val NAME = "@NAME@"
         const val ID = "@ID@"
         const val VER = "@VER@"
+        val modDir = File("./W-OVERFLOW/$NAME")
     }
 }
