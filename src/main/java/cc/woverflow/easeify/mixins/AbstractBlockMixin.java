7d0e13b9f09ffc7950b6652a483939ea588cd350
Maximusbarcz <maxim.baranek@gmail.com>
2022-05-22 20:47:53 +0200

    Added the Remove Ground Foliage option from patcher.

--- src/main/java/cc/woverflow/easeify/mixins/AbstractBlockMixin.java
+++ src/main/java/cc/woverflow/easeify/mixins/AbstractBlockMixin.java
@@ -16,6 +16,7 @@ public class AbstractBlockMixin {
     @Inject(method = "getRenderType", at = @At("HEAD"), cancellable = true)
     public void Easeify$modifyRenderType(BlockState state, CallbackInfoReturnable<BlockRenderType> cir) {
         if (EaseifyConfig.INSTANCE.getRemoveGroundFoliage()) {
+            //No, i wont figure out how to use block tags in this
             if (state.isOf(Blocks.GRASS) || state.isOf(Blocks.TALL_GRASS) || state.isOf(Blocks.DANDELION) || state.isOf(Blocks.POPPY) || state.isOf(Blocks.BLUE_ORCHID) || state.isOf(Blocks.ALLIUM) || state.isOf(Blocks.AZURE_BLUET) || state.isOf(Blocks.ORANGE_TULIP) || state.isOf(Blocks.PINK_TULIP) || state.isOf(Blocks.RED_TULIP) || state.isOf(Blocks.WHITE_TULIP) || state.isOf(Blocks.OXEYE_DAISY) || state.isOf(Blocks.CORNFLOWER) || state.isOf(Blocks.LILY_OF_THE_VALLEY) || state.isOf(Blocks.SUNFLOWER) || state.isOf(Blocks.LILAC) || state.isOf(Blocks.ROSE_BUSH) || state.isOf(Blocks.PEONY) || state.isOf(Blocks.BROWN_MUSHROOM) || state.isOf(Blocks.RED_MUSHROOM)) {
                 cir.setReturnValue(BlockRenderType.INVISIBLE);
             }
