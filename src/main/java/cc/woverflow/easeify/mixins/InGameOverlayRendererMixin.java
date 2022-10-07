14623cc47344e9a916675734aaf9b354e74fa614
MicrocontrollersDev <microcontrollersyt@gmail.com>
2022-10-06 20:40:27 -0500

    Add option to remove fire overlay when invincible

--- src/main/java/cc/woverflow/easeify/mixins/InGameOverlayRendererMixin.java
+++ src/main/java/cc/woverflow/easeify/mixins/InGameOverlayRendererMixin.java
@@ -9,6 +9,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static net.minecraft.entity.effect.StatusEffects.FIRE_RESISTANCE;
+
 @Mixin(InGameOverlayRenderer.class)
 public class InGameOverlayRendererMixin {
     private static boolean hasPushed = false;
@@ -31,5 +33,10 @@ public class InGameOverlayRendererMixin {
             hasPushed = false;
             matrices.pop();
         }
+    }
+
+    @Inject(method = "renderFireOverlay", at = @At("HEAD"), cancellable = true)
+    private static void cancelFireOverlay(MinecraftClient client, MatrixStack matrices, CallbackInfo ci) {
+        if (EaseifyConfig.INSTANCE.getRemoveFireOverlay() && client.player != null && (client.player.isCreative() || client.player.hasStatusEffect(FIRE_RESISTANCE))) ci.cancel();
     }
 }
